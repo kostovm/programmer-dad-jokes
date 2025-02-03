3503aa1b2b7eb187bd04b6531741d8264f07561a
kostovm <kostov543@gmail.com>
2025-02-03 14:29:32 +0200

    Joke: Recursive Hydration
    It is almost never time for beer.

--- dadJokes.ts
+++ dadJokes.ts
@@ -2,5 +2,15 @@
 
 const isRight = true ? 'wife' : 'husband';
 
+// Recursive Hydration
 
+function getDrink(isTimeForBeer: boolean): string{
 
+    if(isTimeForBeer){
+        let drink = 'Beer';
+        return drink;
+    }else{
+        let drink = 'Water';
+        return getDrink(!isTimeForBeer);
+    }
+}
